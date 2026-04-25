c02014856e6eca9f997d142b279b23a379172d21
Rahul Saini <140973573+rahulsaini514715@users.noreply.github.com>
2026-04-25 23:17:40 +0530

    Learn/Understand ngrx-startercode

---
+++ NGRX Practice/ngrx-startercode_lecture_1/src/app/components/grocery/grocery.component.ts
@@ -0,0 +1,53 @@
+import { Component, Signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Grocery } from '../../../models/grocery.model';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { addToBucket, removeFromBucket } from '../../store/actions/bucket.action';
+
+
+@Component({
+  selector: 'app-grocery',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './grocery.component.html',
+  styleUrl: './grocery.component.css'
+})
+export class GroceryComponent {
+
+  // NgRx (aur reactive programming) mein jo variables Observables hote hain unke naam ke end mein hum $ lagate hain — ye ek naming convention hai, syntax ka part nahi.
+
+
+  groceries$?:Observable<Grocery[]>;
+
+  constructor(private store:Store<{groceries:Grocery[]}>){
+    this.groceries$ = store.select("groceries")
+  }
+
+
+  onTypeChange(event: Event){
+
+  }
+
+
+  increment(item:Grocery){
+    const payload = {
+      id:item.id,
+      name:item.name,
+      quantity:1
+    }
+    // this.store.dispatch({type:"Update",payload:payload})
+
+    // this.store.dispatch(addToBucket({id:payload.id,name:payload.name}))
+    this.store.dispatch(addToBucket({payload:payload}))
+
+  }
+  decrement(item:Grocery){
+    const payload = {
+      id:item.id,
+    }
+
+    this.store.dispatch(removeFromBucket({payload}))
+  }
+
+}
