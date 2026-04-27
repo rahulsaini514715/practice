608b46aae16ccc7b6b334fe780c65171ab1eca01
Rahul Saini <140973573+rahulsaini514715@users.noreply.github.com>
2026-04-27 23:49:39 +0530

    Practice: implemented base conversions, exponentiation (normal & fast), and basic number utilities in Java

---
+++ DSA Practice/Practice  Maths for DSA - 1/code/MathsForDSA.java
@@ -0,0 +1,82 @@
+
+public class MathsForDSA {
+
+    public static void main(String[] args) {
+        decimalToAnyBase(6, 2);//decimalToBase
+        System.out.println("...........................");
+        AnyBaseToDecimal(101, 2);//BinarytoDecimal
+        fastExponentiation(2, 3);
+        findEvenOdd(7);
+        reverseNum(20461);
+        power(10, 6);
+    }
+
+    static void decimalToAnyBase(int decimalNum, int base) {
+        int resNum = 0;
+        int power = 0;
+        while (decimalNum > 0) {
+            int rem = decimalNum % base;
+            decimalNum /= base;
+            resNum += rem * power(10, power);
+            power++;
+        }
+        System.out.println("Result is " + resNum);
+    }
+
+    static void AnyBaseToDecimal(int binaryNum, int base) {
+        int resNum = 0;
+        int power = 0;
+        while (binaryNum > 0) {
+            int unitDigit = binaryNum % 10;
+            binaryNum /= 10;
+            resNum += unitDigit * power(base, power);
+            power++;
+        }
+        System.out.println("Result iiis " + resNum);
+    }
+
+    static void findEvenOdd(int num) {
+        if (num % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+
+    static void reverseNum(int num) {
+
+        int revNum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            revNum = revNum * 10 + digit;
+            num /= 10;
+        }
+        System.out.println("Reverse is: " + revNum);
+
+    }
+
+    static double power(int num, int power) {
+        double res = 1;
+        for (int i = 0; i < power; i++) {
+            res = res * num;
+        }
+        System.out.println(res);
+        return res;
+    }
+
+    static double fastExponentiation(int num, int power) {
+        int powr = power;
+        int numb = num;
+        double res = 1;
+        while (power > 0) {
+            if (power % 2 != 0) {
+                res = res * num;
+            }
+            power /= 2;
+            num = num * num;
+        }
+        System.out.println("fastExponentiation : " + " Number - " + numb + " Power- " + powr + " : " + "Output : " + res);
+        return res;
+    }
+
+}
