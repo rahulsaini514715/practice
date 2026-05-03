06c46121493b33fb8928ce5a288f09b2d2585a8f
Rahul Saini <140973573+rahulsaini514715@users.noreply.github.com>
2026-05-03 23:57:54 +0530

    practice: binary search on answer (minimum eating speed), Koko eating bananas problem, and feasibility check using hours calculation logic

---
+++ DSA Practice/Practice - Koko Eating Bananas/KokoEatingBananas.java
@@ -0,0 +1,51 @@
+
+public class KokoEatingBananas {
+
+    public static void main(String[] args) {
+        int[] piles = {3, 6, 7, 11}; // Example pile sizes
+        int h = 8; // Example number of hours
+
+        KokoEatingBananas obj = new KokoEatingBananas(); // Creating an object
+        int result = obj.minEatingSpeed(piles, h); // Calling the function
+        System.out.println("Minimum Eating Speed: " + result);
+
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int start = 1;
+        int end = Integer.MIN_VALUE; // for finding largest value in piles
+
+        // finding max
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > end) {
+                end = piles[i];
+            }
+        }
+        int ans = -1;
+        // apply binary search on the possible values of K -> (1, max(piles))
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isEatingSpeedValid(piles, mid, h)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    boolean isEatingSpeedValid(int piles[], int eatingSpeed, int hours) {
+        int hoursSpent = 0;
+        for (int i = 0; i < piles.length; i++) {
+            hoursSpent += piles[i] / eatingSpeed;
+            if (piles[i] % eatingSpeed != 0) {
+                hoursSpent += 1;
+            }
+            if (hoursSpent > hours) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
