715875faa4ffb6b6196c783ed916101e6af69b93
Rahul Saini <140973573+rahulsaini514715@users.noreply.github.com>
2026-07-07 20:58:35 +0530

    Practice: solve Find All Disappeared Numbers using Cycle Sort

---
+++ DSA Practice/Practice - Find All Numbers Disappeared in Array/code/AllDisappearedNumber.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        Solution solution = new Solution();
+        List<Integer> result = solution.findDisappearedNumbers(nums);
+        System.out.println("Disappeared numbers: " + result);
+    }
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        cycleSort(nums);
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                list.add(i + 1);
+            }
+        }
+        return list;
+
+    }
+
+    public void cycleSort(int nums[]) {
+        int n = nums.length;
+        int index = 0;
+        while (index < n) {
+            int element = nums[index];
+            int correctPos = element - 1;
+            if (nums[index] != nums[correctPos]) {
+                swap(nums, index, correctPos);
+            } else {
+                index++;
+            }
+        }
+    }
+
+    public void swap(int arr[], int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
